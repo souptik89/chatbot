dc0bd0f22d37dab69a205694a13f624c599a7b4a
souptik-ionic <souptik89@gmail.com>
2018-01-04 01:19:41 +0530

    updated

--- js/toogle-menu.js
+++ js/toogle-menu.js
@@ -2,80 +2,81 @@
 
 //navbar open close
 
-$(".menu-opener").click(function () {
-  $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+$(".menu-opener").click(function() {
+    $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
 });
 
 
 //navbar show hide on scroll
-$(document).ready(function () {
+$(document).ready(function() {
 
-  $(window).scroll(function () {
+    $(window).scroll(function() {
 
-    var scrollValue = ($(window).scrollTop());
+        var scrollValue = ($(window).scrollTop());
 
-    console.log(scrollValue)
-    if ($(window).scrollTop() > 200) {
-      $('#nav_bar').show(300);
-    }
-    if ($(window).scrollTop() < 199) {
-      $('#nav_bar').hide(100);
-    }
-  });
+        console.log(scrollValue)
+        if ($(window).scrollTop() > 200) {
+            $('#nav_bar').show(300);
+        }
+        if ($(window).scrollTop() < 199) {
+            $('#nav_bar').hide(100);
+        }
+    });
 });
 
 
 
 //chat tooltip show hide
 
-$(document).ready(function () {
-  $('#boticon').hover(function () {
-    $('#tip').toggle(function () {
-      if ($('#tip').hasClass('slideRightReturn')) {
-        $('#tip').removeClass('slideRightReturn');
-        $('#tip').addClass('slideRight');
-      }
-      if ($('#tip').hasClass('slideRight')) {
-        $('#tip').removeClass('slideRight');
-        $('#tip').addClass('slideRightReturn');
-      }
-    })
-  });
+$(document).ready(function() {
+    $('#boticon').hover(function() {
+        $('#tip').toggle(function() {
+            if ($('#tip').hasClass('slideRightReturn')) {
+                $('#tip').removeClass('slideRightReturn');
+                $('#tip').addClass('slideRight');
+            }
+            if ($('#tip').hasClass('slideRight')) {
+                $('#tip').removeClass('slideRight');
+                $('#tip').addClass('slideRightReturn');
+            }
+        })
+    });
 });
 
 //chat icon blink animation
-$(document).ready(function () {
+$(document).ready(function() {
 
-  setInterval(function () {
-    $('.blink').toggleClass("dot")
-  }, 6000);
+    setInterval(function() {
+        $('.blink').toggleClass("dot")
+    }, 6000);
 });
 
 //chat window show hide
-$(document).ready(function () {
+$(document).ready(function() {
 
-  $('#chat').click(function () {
-    $('#chat-window').toggle(function () {
-      if ($('#chat-window').hasClass('spaceInRight')) {
-        $('#chat-window').removeClass('spaceInRight');
-        $('#chat-window').addClass('spaceOutRight');
-      }
-      if ($('#chat-window').hasClass('spaceOutRight')) {
-        $('#chat-window').removeClass('spaceOutRight');
-        $('#chat-window').addClass('spaceInRight');
-      }
+    $('#chat,#cross').click(function() {
+        $('#chat-window').toggle(function() {
+            if ($('#chat-window').hasClass('spaceInRight')) {
+                $('#chat-window').removeClass('spaceInRight');
+                $('#chat-window').addClass('spaceOutRight');
+            }
+            if ($('#chat-window').hasClass('spaceOutRight')) {
+                $('#chat-window').removeClass('spaceOutRight');
+                $('#chat-window').addClass('spaceInRight');
+            }
+
+        });
 
     });
 
-  })
 });
 
 //chat hint answer horizontal scroll
 
-$(document).ready(function () {
-  $('#ans-hints').mousewheel(function (e, delta) {
-    this.scrollLeft -= (delta * 20);
-    e.preventDefault();
-  });
+$(document).ready(function() {
+    $('#ans-hints').mousewheel(function(e, delta) {
+        this.scrollLeft -= (delta * 20);
+        e.preventDefault();
+    });
 
 });
